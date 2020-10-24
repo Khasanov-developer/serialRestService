55c1cfc7f966abb90f73aec3940db9eb8139f3ef
r.khasanov <r.khasanov>
2020-10-24 20:17:58 +0500

    RestController test

--- src/main/java/presentation/rest/SerialRestController.java
+++ src/main/java/presentation/rest/SerialRestController.java
@@ -1,6 +1,8 @@
 package presentation.rest;
 
+import entity.dto.Serial;
 import entity.repo.SerialRepository;
+
 import static spark.Spark.*;
 
 public class SerialRestController {
@@ -10,7 +12,14 @@ public class SerialRestController {
 
     public SerialRestController(SerialRepository repository) {
         this.repository = repository;
-        get(path, (request, response) -> repository.getSerialByName(""), JsonUtil.json());
-    }
+        get(path, (req, res) -> {
+            String name = req.queryMap().get("serial").get("name").value().replace("_", " ");
+            Serial serial = repository.getSerialByName(name);
+            return serial;
+        }, JsonUtil.json());
 
+        get(path.concat("/all"), (req, res) -> {
+            return repository.getAll();
+        }, JsonUtil.json());
+    }
 }
