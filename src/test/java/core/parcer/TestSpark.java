dfe8250a43ca2c3ee495a2666b33a4551094319c
Oleg <ddiavona@gmail.com>
2020-10-24 11:58:23 +0500

    Создал класс TestSpark и добавил в Maven - Spark

---
+++ src/test/java/core/parcer/TestSpark.java
@@ -0,0 +1,38 @@
+package core.parcer;
+
+import core.hibernate.HibernateEntityManagerFactory;
+import entity.dto.Serial;
+import entity.repo.SerialRepositoryImpl;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+
+import static spark.Spark.get;
+import static spark.Spark.port;
+
+public class TestSpark {
+    private static EntityManager em;
+
+    public TestSpark() {
+        em = HibernateEntityManagerFactory.getEntityManager();
+    }
+
+    public static void main(String[] args) {
+        port(5432);
+        String name1 = "Danger";
+        get("/hello", (req, res) -> getSerialByName(name1));
+    }
+
+    public static Serial getSerialByName(String name) {
+        List<Serial> serials = em.createQuery("SELECT s FROM Serial s WHERE s.name = :name", Serial.class)
+                .setParameter("name", name)
+                .getResultList();
+
+        if (serials.isEmpty()) {
+            return null;
+        } else {
+            return serials.get(0);
+        }
+    }
+}
