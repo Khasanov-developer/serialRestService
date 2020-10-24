e167515a22b939d8fe3154fdc254acf9ace8af71
r.khasanov <r.khasanov>
2020-10-24 18:34:25 +0500

    добавил JsonUtil

---
+++ src/main/java/presentation/rest/JsonUtil.java
@@ -0,0 +1,15 @@
+package presentation.rest;
+
+import com.google.gson.Gson;
+import spark.ResponseTransformer;
+
+class JsonUtil {
+
+    public static String toJson(Object object) {
+        return new Gson().toJson(object);
+    }
+
+    public static ResponseTransformer json() {
+        return JsonUtil::toJson;
+    }
+}
